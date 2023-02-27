e0eb32f713496b5fc629ea8b40e99917589852cb
jameskalfox <75384370+jameskalfox@users.noreply.github.com>
2023-02-27 12:37:14 +0000

    Development

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,11 +15,9 @@ streamlit.header('Zena\'s Amazing Athleisure Catalog')
 
 option = streamlit.selectbox('Pick a sweatsuit colour or style:', colour_list)
 
-streamlit.image
-streamlit.write('Our ' + option + ' sweatsuit')
-
 my_cur.execute(
     'select direct_url from catalog_for_website where color_or_style = \'' + option + '\'')
 df2 = my_cur.fetchone()
 
 streamlit.image(df2[0], width=400)
+streamlit.write('Our ' + option + ' sweatsuit')
