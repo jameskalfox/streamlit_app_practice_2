c7f7935bbc1aea0f658750e5ba127ee96bbb078b
jameskalfox <75384370+jameskalfox@users.noreply.github.com>
2023-02-27 12:02:53 +0000

    initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+import streamlit
+import snowflake.connector
+import pandas
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets['snowflake'])
+my_cur = my_cnx.cursor()
+
+my_cur.execute('select color_or_style from catalog_for_website')
+my_catalog = my_cur.fetchall()
+
+df = pandas.dataframe(my_catalog)
+streamlit.header('Zena\'s Amazing Athleisure Catalog')
+
+streamlit.selectbox('Pick a sweatsuit colour or style:', list(my_catalog))
